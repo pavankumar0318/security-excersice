825eba7207631f9bdff61fb099801b73d4db925d
pavankumar <p.pavankkumar.p@gmail.com>
2024-02-21 00:36:55 +0530

    Created Project

---
+++ Security/src/main/java/io/exersice/Security/DemoSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package io.exersice.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityProjectApplication.class, args);
+	}
+
+}
---
+++ Security/src/test/java/io/exersice/Security/DemoSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.exersice.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
